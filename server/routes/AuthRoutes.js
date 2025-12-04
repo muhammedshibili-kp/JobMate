15a502607a3253c0ede929c3ec3d39c649aa5f41
muhammedshibili-kp <muhammedshibili7425@gmail.com>
2025-12-04 15:57:10 +0530

    Added authentication

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { registerUser } from '../controllers/AuthController.js'
-
+import { getUserData, loginUser, registerUser } from '../controllers/AuthController.js'
+import { protect } from '../middleware/AuthMiddleware.js'
 
 const authRouter = express.Router()
 
 
 authRouter.post('/register',registerUser)
+authRouter.post('/login',loginUser)
+authRouter.post('/me',protect,getUserData)
 
 
 export default authRouter
